984568f99cf4f2efba5e160f2d3a4e1b7d695d06
Tattvam <vaidyats@robodogs.org>
2026-02-03 15:54:19 -0500

    ran npm build for updated doc file again

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/tattvamvaidya.github.io/',
+  build: {
+    outDir: 'docs',
+  },
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
