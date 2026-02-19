b4e6d9c4c8183d10f747d84b800601daecdf24ef
Zambucko2 <tattvam.vaidya@gmail.com>
2026-02-18 22:49:46 -0500

    bruhhhh

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/TattvamVaidya/',
   build: {
-    outDir: 'docs/',
+    outDir: 'docs',
   },
   plugins: [react()],
   optimizeDeps: {
