3ffa28fb43b522c48f0ff0b59cce337ab104914c
Zambucko2 <tattvam.vaidya@gmail.com>
2026-02-18 22:31:57 -0500

    I fixed it 90% sure this time

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/tattvamvaidya.github.io/',
+  base: '/TattvamVaidya/',
   build: {
     outDir: 'docs/',
   },
